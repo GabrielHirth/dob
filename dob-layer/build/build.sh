1c1bbf6209f5d684cb5084b37a1c70f3e8183ac4
DOB <dob@local>
2026-08-24 21:28:13 -0300

    fix: ports uses main branch (no releng/14.2 in ports repo)

--- dob-layer/build/build.sh
+++ dob-layer/build/build.sh
@@ -16,7 +16,7 @@ if [ ! -d /usr/src ]; then
     git clone --branch "releng/${DOB_FREEBSD_REL}" https://git.FreeBSD.org/src.git /usr/src
 fi
 if [ ! -d /usr/ports ]; then
-    git clone --branch "releng/${DOB_FREEBSD_REL}" https://git.FreeBSD.org/ports.git /usr/ports
+    git clone --branch main https://git.FreeBSD.org/ports.git /usr/ports
 fi
 
 # 2. Build x86_64 ISO via release.sh with DOB config
